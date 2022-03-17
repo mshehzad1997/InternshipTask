9a1103f1afffffb00783e6ad5d01cfb2b8b75e7d
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-17 18:37:58 +0500

    GetEmptyRole

---
+++ InternshipTask/Models/ManageRoles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipTask.Models
+{
+    public class ManageRoles
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+        public Payment Payments { get; set; }
+        public ManageTenant ManageTenants { get; set; }
+        public DemoRequest DemoRequests { get; set; }
+        public enum Payment
+        {
+            ViewPayment,
+            ChangeStatus
+
+        }
+        public enum ManageTenant
+        {
+            RegisterTenant,
+            ViewTenant,
+            UpdateTenant,
+            UpgradePlan,
+            RessetPassword
+        }
+        public enum DemoRequest
+        {
+            ViewRequest,
+            ApproveRequest
+        }
+    }
+}
